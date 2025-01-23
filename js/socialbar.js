6d156ac235f248f75e792a9ef5e290949504f3bb
Emerson50300 <tu.email@ejemplo.com>
2025-01-23 18:31:39 +0100

    Agregado script de SocialBar a las páginas principales

---
+++ js/socialbar.js
@@ -0,0 +1,7 @@
+// SocialBar Script
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl25657425.profitablecpmrate.com/dc/a4/a5/dca4a59f516bf4c2540f99d928cfda7c.js';
+    document.body.appendChild(script);
+})();
